99c945a1c88106f4858a45769caa9bb1bba2fb5f
Liu Siyuan <liusy182@gmail.com>
2017-05-19 15:35:15 +0800

    stepper motor io

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
+stepper_motor_out = {
+  'ENA': 11,
+  'IN1': 13,
+  'IN2': 15,
+  'IN3': 12,
+  'IN4': 16,
+  'ENB': 18
+}
+
+stepper_motor_in = {
+  21: 'IN1',
+  22: 'IN2',
+  23: 'IN3',
+  24: 'IN4'
+}
+
 def setup():
   GPIO.setmode(GPIO.BOARD)
-
-  # Define the GPIO input ports for IR sensors
-  input_chan_list = [8, 10, 12]
-  GPIO.setup(input_chan_list, GPIO.IN)
-
-  # Define the GPIO output ports for controllering LEDS
-  output_chan_list = [22, 24, 26]
-  GPIO.setup(output_chan_list, GPIO.OUT, initial=GPIO.HIGH)
-
+  GPIO.setup(stepper_motor_in.keys(), GPIO.IN)
+  GPIO.setup(stepper_motor_out.values(), GPIO.OUT, initial=GPIO.HIGH)
 
 def run_loop():
     while True:
-      time.sleep(1)
+      for key, val in stepper_motor_in:
+        if GPIO.input(key):
+          GPIO.output(stepper_motor_out[val], GPIO.HIGH)
+        else:
+          GPIO.output(stepper_motor_out[val], GPIO.LOW)
+      time.sleep(0.1)
+
 
 if __name__ == "__main__":
   setup()
