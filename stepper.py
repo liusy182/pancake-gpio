3141365af73e6ec4bab8f555e5c356b11b6e8d92
liusy182 <liusy182@hotmail.com>
2017-06-06 03:52:49 +0000

    updates

--- stepper.py
+++ stepper.py
@@ -10,16 +10,21 @@ out = {
   'ENB': 18
 }
 
-Direction = 1
+Direction = 0
 Steps = 0
 steps_left = 4095
 
 def reset():
-  Direction = 1
+  global Direction
+  global Steps
+  global steps_left
+  Direction = 0
   Steps = 0
   steps_left = 4095
 
 def set_direction():
+  global Steps
+  global Direction
   if Direction == 1:
     Steps += 1
   if Direction == 0:
@@ -30,41 +35,43 @@ def set_direction():
     Steps = 7
 
 def step():
+  global Steps
+  print(Steps)
   if Steps == 0:
-    GPIO.output(out['IN1'], GPIO.LOW) 
-    GPIO.output(out['IN2'], GPIO.LOW)
+    GPIO.output(out['IN1'], GPIO.HIGH) 
+    GPIO.output(out['IN2'], GPIO.HIGH)
     GPIO.output(out['IN3'], GPIO.LOW)
-    GPIO.output(out['IN4'], GPIO.HIGH)
+    GPIO.output(out['IN4'], GPIO.LOW)
   elif Steps == 1:
     GPIO.output(out['IN1'], GPIO.LOW) 
-    GPIO.output(out['IN2'], GPIO.LOW)
+    GPIO.output(out['IN2'], GPIO.HIGH)
     GPIO.output(out['IN3'], GPIO.HIGH)
-    GPIO.output(out['IN4'], GPIO.HIGH)
+    GPIO.output(out['IN4'], GPIO.LOW)
   elif Steps == 2:
     GPIO.output(out['IN1'], GPIO.LOW) 
     GPIO.output(out['IN2'], GPIO.LOW)
     GPIO.output(out['IN3'], GPIO.HIGH)
-    GPIO.output(out['IN4'], GPIO.LOW)
+    GPIO.output(out['IN4'], GPIO.HIGH)
   elif Steps == 3:
-    GPIO.output(out['IN1'], GPIO.LOW) 
-    GPIO.output(out['IN2'], GPIO.HIGH)
-    GPIO.output(out['IN3'], GPIO.HIGH)
-    GPIO.output(out['IN4'], GPIO.LOW)
+    GPIO.output(out['IN1'], GPIO.HIGH) 
+    GPIO.output(out['IN2'], GPIO.LOW)
+    GPIO.output(out['IN3'], GPIO.LOW)
+    GPIO.output(out['IN4'], GPIO.HIGH)
   elif Steps == 4:
-    GPIO.output(out['IN1'], GPIO.LOW) 
+    GPIO.output(out['IN1'], GPIO.HIGH) 
     GPIO.output(out['IN2'], GPIO.HIGH)
     GPIO.output(out['IN3'], GPIO.LOW)
     GPIO.output(out['IN4'], GPIO.LOW)
   elif Steps == 5:
-    GPIO.output(out['IN1'], GPIO.HIGH) 
+    GPIO.output(out['IN1'], GPIO.LOW) 
     GPIO.output(out['IN2'], GPIO.HIGH)
-    GPIO.output(out['IN3'], GPIO.LOW)
+    GPIO.output(out['IN3'], GPIO.HIGH)
     GPIO.output(out['IN4'], GPIO.LOW)
   elif Steps == 6:
-    GPIO.output(out['IN1'], GPIO.HIGH) 
+    GPIO.output(out['IN1'], GPIO.LOW) 
     GPIO.output(out['IN2'], GPIO.LOW)
-    GPIO.output(out['IN3'], GPIO.LOW)
-    GPIO.output(out['IN4'], GPIO.LOW)
+    GPIO.output(out['IN3'], GPIO.HIGH)
+    GPIO.output(out['IN4'], GPIO.HIGH)
   elif Steps == 7:
     GPIO.output(out['IN1'], GPIO.HIGH) 
     GPIO.output(out['IN2'], GPIO.LOW)
@@ -78,9 +85,11 @@ def step():
   set_direction()
 
 def loop():
+  global steps_left
   print(steps_left)
   while steps_left > 0:
+    print(steps_left)
     step()
-    time.sleep(0.1)
+    time.sleep(0.005)
     steps_left -= 1
 
