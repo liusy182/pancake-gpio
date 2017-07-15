a2ce0c3587912f68053c206d25c11b371dbe4e3d
xlk3099 <xlk3099@gmail.com>
2017-07-15 16:29:41 +0800

    testing 1

--- pancake/stepper_motor_test.py
+++ pancake/stepper_motor_test.py
@@ -30,8 +30,12 @@ def setup():
 
 if __name__ == "__main__":
     setup()
+    print("x forwaring 200")
     motorX.forward(0.005, 200)
+    print("x backwarding 200")
     motorX.backward(0.005, 200)
-    motorY.forward(0.005, 200)
+    print("y forwaring 200")
+    motorY.forward(0.005, 200)    
+    print("y backwarding 200")
     motorY.backward(0.005,200)
     GPIO.cleanup()
